2a321642a0326ee490a9010ddf3ca08cffd65058
Mithun.U <116871732+Mithun-750@users.noreply.github.com>
2024-02-15 22:13:54 +0530

    create initial version for home page

---
+++ client/src/components/Home/Domains.jsx
@@ -0,0 +1,64 @@
+import React, { useRef, useLayoutEffect } from 'react';
+import { Separator } from '../ui/separator';
+import Styles from '@/styles/domains.module.css';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const Domains = () => {
+    const container = useRef(null);
+    const back = useRef([]);
+
+    useLayoutEffect(() => {
+        const context = gsap.context(() => {
+            const tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: container.current,
+                    start: 'top center',
+                    stop: 'top top',
+                    scrub: true,
+                },
+            });
+
+            back.current.forEach((ref) => {
+                tl.to(ref, {
+                    backgroundPositionY: +0.5,
+                    duration: 2,
+                    ease: 'Power2.out',
+                }, 0);
+            });
+        });
+
+        return () => context.revert();
+    }, []);
+
+    return (
+        <div ref={container} className="bg-gray-950 py-8 w-screen my-10 text-gray-100 text-center">
+            <h1 className="text-3xl my-5">Domains</h1>
+            <Separator className="my-5 bg-white/60" />
+            <div className="flex flex-wrap justify-around py-12 px-5 items-center">
+                {[
+                    { style: Styles.cyberBackground, text: 'Cyber Security' },
+                    { style: Styles.cgBackground, text: 'Computer Graphics' },
+                    { style: Styles.ssBackground, text: 'System Software' },
+                    { style: Styles.dsBackground, text: 'Distributed Systems' },
+                ].map((domain, index) => (
+                    <div
+                        key={index}
+                        ref={(ref) => (back.current[index] = ref)}
+                        className={`w-fit h-fit rounded ${domain.style} mt-7`}
+                    >
+                        <a href="#">
+                            <div className="py-7 rounded md:w-fit mt-0 px-5 text-xl backdrop-blur-sm bg-black/30">
+                                {domain.text}
+                            </div>
+                        </a>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Domains;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,113 @@
+"use client"
+import React, { useEffect, useState } from 'react'
+import { Button } from './ui/button'
+import Image from 'next/image'
+import { usePathname, useRouter } from 'next/navigation'
+
+
+
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const pathname = usePathname()
+
+    const [solidNav, SetsolidNav] = useState(false)
+
+    const handleNavOpen = () => {
+        if (isOpen) {
+            setIsOpen(false)
+            if (solidNav && (window.scrollY < 200)) {
+                SetsolidNav(false)
+            }
+        } else {
+            setIsOpen(true)
+            if (!solidNav) {
+                SetsolidNav(true)
+            }
+
+        }
+    }
+
+    const router = useRouter();
+
+    const alternav = () => {
+        if (window.scrollY >= 200) {
+            SetsolidNav(true);
+        } else {
+            SetsolidNav(false);
+        }
+    };
+
+
+    useEffect(() => {
+        if (pathname === '/') {
+
+            window.addEventListener('scroll', alternav);
+        } else {
+            SetsolidNav(true);
+        }
+
+        console.log(pathname)
+    }, [pathname]);
+
+
+
+
+
+
+    return (
+        <header className={`fixed z-10 text-xl transition duration-500 w-screen py-4 top-0 left-0 flex justify-center items-center ${solidNav ? 'backdrop-blur-sm bg-zinc-950/50' : ' bg-transparent'}   ${isOpen ? 'h-fit py-10' : 'h-16'}`}>
+            <div className="mx-auto w-full px-4 sm:px-6 lg:px-8">
+                <div className={`flex items-center justify-between ${isOpen ? 'flex-col' : 'md:flex md:items-center md:gap-12'}`}>
+                    <div className="flex-1 md:flex md:items-center md:gap-12">
+                        <a className="block text-white " href="#">
+                            <Image
+                                src='https://github.com/Enigma-IIITS/enigma-site/assets/116871732/c72bc255-b90b-45f6-b980-b7b451d6465c'
+                                alt="logo"
+                                width={50}
+                                height={50}
+                            />
+                        </a>
+                    </div>
+
+                    <div className={`${isOpen ? 'w-full mt-4 flex flex-col items-center justify-between' : 'md:flex md:items-center md:gap-12'} md:min-w-[40%]`}>
+                        <nav aria-label="Global" className={`${isOpen ? 'block' : 'hidden md:block'} md:w-full`}>
+                            <ul className={`${isOpen ? 'flex flex-col items-center gap-6 text-sm' : 'flex flex-row items-center gap-6 text-sm md:flex-row'} md:w-full md:flex md:justify-between`}>
+                                <li>
+                                    <a className="text-gray-300 hover:text-gray-400/75 transition " href="#">About</a>
+                                </li>
+                                <li>
+                                    <a className="text-gray-300 hover:text-gray-400/75 transition " href="#">Domain</a>
+                                </li>
+                                <li>
+                                    <a className="text-gray-300 hover:text-gray-400/75 transition " href="#">Projects</a>
+                                </li>
+                                <li>
+                                    <a className="text-gray-300 hover:text-gray-400/75 transition " href="#">Team</a>
+                                </li>
+                                <li>
+                                    <a className="text-gray-300 hover:text-gray-400/75 transition " href="#">Blog</a>
+                                </li>
+                            </ul>
+                        </nav>
+
+                        <div className={`flex items-center gap-4 w-fit ${isOpen ? "mt-4" : " "} md:hidden`}>
+
+
+                            <div className="block ">
+
+                                <Button onClick={handleNavOpen} >
+                                    <span className="material-symbols-outlined">
+                                        {isOpen ? "close" : "menu"}
+                                    </span>
+                                </Button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Navbar
