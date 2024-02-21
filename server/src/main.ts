604c9a5a4366d887fc128e1c7c86b63b1291cc2d
aahnik <daw@aahnik.dev>
2024-02-21 13:50:06 +0530

    remove lock icon for routes having public decorator in swagger docs

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { OpenAPIObject } from '@nestjs/swagger';
 
 declare const module: any;
 
@@ -11,9 +12,22 @@ async function bootstrap() {
     .setTitle('Enigma Backend API')
     .setDescription('Enigma: The Core Computer Science Club')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
+
+  Object.values((document as OpenAPIObject).paths).forEach((path: any) => {
+    Object.values(path).forEach((method: any) => {
+      if (
+        Array.isArray(method.security) &&
+        method.security.includes('public')
+      ) {
+        method.security = [];
+      }
+    });
+  });
+
   SwaggerModule.setup('docs', app, document);
 
   await app.listen(3000);
