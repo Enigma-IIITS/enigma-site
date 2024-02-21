3bceee28df692edafd8d9084fc80f8dc328840a7
aahnik <daw@aahnik.dev>
2024-02-21 13:49:38 +0530

    create get status dto for root route

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './auth/public';
+import { GetStatusDto } from './dto/get-status.dto';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
+@ApiBearerAuth()
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getStatus(): GetStatusDto {
+    return this.appService.getStatus();
   }
 }
