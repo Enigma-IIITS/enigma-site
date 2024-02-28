713d7248df78712d8bda0a243c33ad148141eab7
aahnik <daw@aahnik.dev>
2024-02-29 00:37:26 +0530

    update datatype of dateRrange in find all events dto

--- server/src/events/dto/find-all-events.dto.ts
+++ server/src/events/dto/find-all-events.dto.ts
@@ -1,10 +1,10 @@
+import { DateRange } from 'src/entities/date-range.entity';
 import { FindAllDto } from '../../dto/find-all.dto';
 
 export class FindEventsFilter {
   completed?: boolean;
   acceptingRegistrations?: boolean;
-  dateRangeBegin?: Date;
-  dateRangeEnd?: Date;
+  dateRange?: DateRange;
 }
 
 export default class FindAllEventsDto extends FindAllDto {
